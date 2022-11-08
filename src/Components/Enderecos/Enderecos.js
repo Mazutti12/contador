fba8b00d17e88f7fccba4e5d499189f4a846c472
Victor Mazutti <victor.150305@bento.local>
2022-11-08 15:05:15 -0300

    Criando para cidades

--- src/Components/Enderecos/Enderecos.js
+++ src/Components/Enderecos/Enderecos.js
@@ -1,16 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 export const Enderecos = () => {
 
-    const [estados, setEstados]= useState(null)
-    const [estados, setEstados]= useState([{sigla: 'RS', nome:'Rio Grande do Sul'}])
+    const [estado, setEstado] = useState('')
+    const [estados, setEstados] = useState([])
+    const [cidade, setCidade] = useState('')
+    const [cidades, setCidades] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [loadingCidades, setLoadingCidades] = useState(true)
+
+    const handleChange = ({ target: { value } }) => {
+        setEstado(value)
+    }
+
+    const label = useMemo(() => loading ? 'Aguarde...' : 'Selecione', [loading])
+
+    useEffect(() => {
+        const loadEstados = async () => {
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`)
+            const data = await response.json()
+            console.log(data)
+            setEstados(data)
+        }
+        loadEstados()
+        setLoading(false)
+
+
+        // if (loading) return <p>Aguarde...</p>
+
+    }, [])
+
+    useEffect(() => {
+        setLoadingCidades(true)
+        const loadCidades = async () => {
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/municipios`)
+            const data = await response.json()
+            console.log(data)
+            setCidades(data)
+        }
+        loadCidades()
+        setLoadingCidades(false)
+
+    }, [estado])
 
     return (
         <>
-            <select>
-                {estados.map(estado => <option>{estado.nome}</option>)}        
-            </select>
-            <span>Cidades:</span>
+            <div>
+                <span>Estados:</span>
+                <select disabled={loading} value={estado} onChange={handleChange}>
+                    <option value="">{label}</option>
+                    {estados.map(({ id, sigla, nome }) => <option key={id} value={sigla}>{nome}</option>)}
+                </select>
+            </div>
+
+            <div>
+                <span>Cidades:</span>
+                <select disabled={loading} value={cidade} onChange={handleChange}>
+                    <option value="">{label}</option>
+                    {cidades.map(({ id, sigla, nome }) => <option key={id} value={sigla}>{nome}</option>)}
+                </select>
+            </div>
         </>
     )
 }
